42311c54030a9bab7a8059e12f029c3534c89add
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2017-04-21 23:31:40 +0200

    Add incidency matrix implementation

---
+++ SDiZO Zadanie 2/SDiZO Zadanie 2/IncidenceMatrix.cpp
@@ -0,0 +1,93 @@
+#include "stdafx.h"
+#include "IncidenceMatrix.h"
+
+//Base constructor, creates matrix with 10 vertices and 10 edges
+IncidenceMatrix::IncidenceMatrix()
+	: vertices_(10)
+	, edges_(10)
+	, edge_count_(0)
+{
+	//Create Matrix
+	matrix_ = new int *[10];
+
+	//Create array to hold the weights of edges
+	weights = new unsigned int[10]();
+
+	for (int i = 0; i < 10; i++)
+	{
+		matrix_[i] = new int[10];
+		//Mark all rows as NO_EDGE
+		for (int j = 0; j < 10; j++)
+		{
+			matrix_[i][j] = NO_EDGE;
+		}
+	}
+}
+
+//Creates incidence matrix with given number vertices and edges
+IncidenceMatrix::IncidenceMatrix(int number_of_vertices, int number_of_edges)
+	: vertices_(number_of_vertices)
+	, edges_(number_of_edges)
+	, edge_count_(0)
+{
+	//Create Matrix
+	matrix_ = new int *[number_of_vertices];
+	//Create array to hold the weights of edges
+	weights - new unsigned int[number_of_edges]();
+
+	for (int i = 0; i < number_of_vertices; i++)
+	{
+		matrix_[i] = new int[number_of_edges];
+		//Mark all rows as NO_EDGE
+		for (int j = 0; j < number_of_edges; j++)
+		{
+			matrix_[i][j] = NO_EDGE;
+		}
+	}
+}
+
+//Free the memory
+IncidenceMatrix::~IncidenceMatrix()
+{
+	for (int i = 0; i < vertices_;i++)
+	{
+		delete[] matrix_[i];
+	}
+}
+
+void IncidenceMatrix::AddEdge(unsigned int origin, unsigned int destination, unsigned int weight)
+{
+	if (IsValidEdge(origin, destination))
+	{
+		//Connect the origin and destination with edge of given weight
+
+		matrix_[origin][edge_count_] = START;
+		matrix_[destination][edge_count_] = END;
+		weights[edge_count_] = weight;
+		
+		++edge_count_;
+	}
+}
+
+void IncidenceMatrix::Display()
+{
+
+	std::cout << "  ";
+	for (int i = 1; i < edges_ + 1; i++)
+		std::cout << i << " ";
+
+	std::cout << std::endl;
+
+	for (int row = 0; row < vertices_; row++)
+	{
+		std::cout << row << " ";
+		for (int column = 0; column < edges_; column++)
+			std::cout << matrix_[row][column] << " ";
+		std::cout << std::endl;
+	}
+}
+
+bool IncidenceMatrix::IsValidEdge(unsigned int origin, unsigned int destination)
+{
+	return (origin > vertices_ || destination > vertices_) ? false : true;
+}
---
+++ SDiZO Zadanie 2/SDiZO Zadanie 2/IncidenceMatrix.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <iostream>
+
+class IncidenceMatrix
+{
+public:
+	IncidenceMatrix();
+	IncidenceMatrix(int vertices_count, int edges_count);
+	~IncidenceMatrix();
+
+	void AddEdge(unsigned int origin, unsigned int destination, unsigned int weight);
+	void Display();
+private:
+
+	const int START = 1;
+	const int END = -1;
+	const int NO_EDGE = 0;
+
+	
+	int **matrix_;
+	unsigned int vertices_;
+	unsigned int edges_;
+	unsigned int edge_count_;
+	unsigned int *weights;
+
+	bool IsValidEdge(unsigned int origin, unsigned int destination);
+
+};
+
