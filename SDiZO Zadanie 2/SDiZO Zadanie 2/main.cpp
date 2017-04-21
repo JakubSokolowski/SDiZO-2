6ed0a9b83a5f749544b0a15ac37834dc6450b872
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2017-04-22 00:02:03 +0200

    Add testing od Incidence Matrix

--- SDiZO Zadanie 2/SDiZO Zadanie 2/main.cpp
+++ SDiZO Zadanie 2/SDiZO Zadanie 2/main.cpp
@@ -2,10 +2,20 @@
 //
 
 #include "stdafx.h"
+#include "IncidenceMatrix.h"
 
 
 int main()
 {
+	IncidenceMatrix matr = IncidenceMatrix(4, 5);
+	matr.AddEdge(0, 3, 6);
+	matr.AddEdge(1, 0, 5);
+	matr.AddEdge(1, 3, 7);
+	matr.AddEdge(1, 2, 24);
+	matr.AddEdge(2, 0, 9);
+	matr.DisplayMatrix();
+	matr.DisplayWeights();
+	getchar();
     return 0;
 }
 
