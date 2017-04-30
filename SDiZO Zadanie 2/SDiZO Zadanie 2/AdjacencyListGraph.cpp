609e986ee492c8ff841e542fcc0398d86a10f34f
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2017-04-30 18:06:54 +0200

    Add AdjacencyListGraph representation

---
+++ SDiZO Zadanie 2/SDiZO Zadanie 2/AdjacencyListGraph.cpp
@@ -0,0 +1,46 @@
+#include "stdafx.h"
+#include "AdjacencyListGraph.h"
+
+using namespace sdz;
+
+AdjacencyListGraph::AdjacencyListGraph()
+{
+	adj_tab_ = new dts::List<Vertex>[10];
+}
+
+sdz::AdjacencyListGraph::AdjacencyListGraph(uint vertices,bool is_directed)
+{
+	
+	vertices_ = vertices;
+	is_directed_ = is_directed;
+	adj_tab_ = new dts::List<Vertex>[vertices_];
+}
+
+
+AdjacencyListGraph::~AdjacencyListGraph()
+{
+}
+
+void sdz::AdjacencyListGraph::AddConnection(uint v1, uint v2, uint weight)
+{
+	Vertex ver = Vertex(v2, weight);
+	adj_tab_[v1].PushBack(ver);
+	if (!is_directed_)
+		adj_tab_[v2].PushBack(ver);
+}
+
+void sdz::AdjacencyListGraph::DisplayGraph()
+{
+	for (uint it = 0; it < vertices_; it++)
+	{
+		std::cout << "Connections for vertex " << it << std::endl;
+		DisplayConnections(it);
+	}
+}
+
+void sdz::AdjacencyListGraph::DisplayConnections(uint v)
+{
+	for (auto it = adj_tab_[v].begin(); it != adj_tab_[v].end(); it++)
+		std::cout<< it->vertex_id_ <<"-"<< it->edge_weight_<<" "; 
+	std::cout << std::endl;
+}
---
+++ SDiZO Zadanie 2/SDiZO Zadanie 2/AdjacencyListGraph.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "Vertex.h"
+#include "List.h"
+
+	
+namespace sdz
+{
+	class AdjacencyListGraph
+	{
+	public:
+
+		
+		AdjacencyListGraph();
+		AdjacencyListGraph(uint vertices, bool is_directed);
+		~AdjacencyListGraph();
+
+		void AddConnection(uint v1, uint id, uint weight);
+		void DisplayGraph();
+		void DisplayConnections(uint v);
+	private:
+
+		bool is_directed_;
+		uint vertices_;
+		dts::List<Vertex> *adj_tab_;
+
+	};
+
+
+
+}
