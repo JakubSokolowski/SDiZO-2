ec04414aefa372c0fb8bd068c7daa3ba1a102529
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2017-05-02 02:03:55 +0200

    Add tests of BFT

--- SDiZO Zadanie 2/SDiZO Zadanie 2/main.cpp
+++ SDiZO Zadanie 2/SDiZO Zadanie 2/main.cpp
@@ -2,7 +2,6 @@
 //
 
 #include "stdafx.h"
-#include "Vector.h"
 #include <cstdio>
 #include <utility>
 #include <ctime>
@@ -10,10 +9,11 @@
 #include "AdjacencyListGraph.h"
 #include "AdjacencyMatrixGraph.h"
 #include "Vertex.h"
+#include "Queue.h"
 
 
 
-using namespace sdz;
+using namespace SDZ;
 
 int main()
 {
@@ -26,16 +26,42 @@ int main()
 	matr1.DisplayMatrix();
 	matr1.DisplayWeights();
 
-	AdjacencyListGraph matr = AdjacencyListGraph(4,true);
-	matr.AddEdge(0, 3, 6);
-	matr.AddEdge(1, 0, 5);
-	matr.AddEdge(1, 3, 7);
-	matr.AddEdge(1, 2, 24);
-	matr.AddEdge(2, 0, 9);
+	AdjacencyListGraph matr = AdjacencyListGraph(14,true);
+	matr.AddEdge(0, 1, 6);
+	matr.AddEdge(0, 2, 5);
+	matr.AddEdge(0, 8, 7);
+	matr.AddEdge(1, 4, 24);
+	matr.AddEdge(1, 5, 9);
+	matr.AddEdge(1, 7, 9);
+	matr.AddEdge(2, 9, 6);
+	matr.AddEdge(3, 0, 5);
+	matr.AddEdge(3, 10, 7);
+	matr.AddEdge(3, 11, 24);
+	matr.AddEdge(4, 13, 9);
+	matr.AddEdge(5, 6, 9);
+	matr.AddEdge(5, 7, 6);
+	matr.AddEdge(5, 13, 5);
+	matr.AddEdge(7, 8, 7);
+	matr.AddEdge(7, 9, 24);
+	matr.AddEdge(10, 9, 9);
+	matr.AddEdge(10, 11, 9);
+	matr.AddEdge(12, 0, 9);
+	matr.AddEdge(12, 3, 9);
+	matr.AddEdge(13, 12, 9);
 	matr.DisplayGraph();
-
+	matr.BDF(0);
 	getchar();
 
+	
+
+
+
+	//std::cout << queue.GetFront().vertex_id_;
+	//std::cout << queue.GetFront().vertex_id_;
+
+	//std::cout << queue.GetSize();
+
+	getchar();
     return 0;
 
 }
