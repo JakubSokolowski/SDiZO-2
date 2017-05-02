7be0fd75509bae6ea41dbaddcc5ae234285bc858
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2017-05-02 02:04:11 +0200

    Rename dts to DTS

--- SDiZO Zadanie 2/SDiZO Zadanie 2/IncidenceMatrixGraph.cpp
+++ SDiZO Zadanie 2/SDiZO Zadanie 2/IncidenceMatrixGraph.cpp
@@ -1,7 +1,7 @@
 #include "stdafx.h"
 #include "IncidenceMatrixGraph.h"
 
-using namespace sdz;
+using namespace SDZ;
 
 //Base constructor, creates matrix with 10 vertices and 10 edges
 IncidenceMatrixGraph::IncidenceMatrixGraph()
