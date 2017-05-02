b37e2279d84e6c613875efb80f875a6f1213146c
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2017-05-02 02:03:23 +0200

    Implement vertex logic

--- SDiZO Zadanie 2/SDiZO Zadanie 2/Vertex.cpp
+++ SDiZO Zadanie 2/SDiZO Zadanie 2/Vertex.cpp
@@ -1,15 +1,41 @@
 #include "stdafx.h"
 #include "Vertex.h"
 
-sdz::Vertex::Vertex()
+SDZ::Vertex::Vertex()
 	: vertex_id_(0)
-	, edge_weight_(0)
+	, list_()
+	, visited_(false)
 {}
 
-sdz::Vertex::Vertex(uint id, uint weight)
+SDZ::Vertex::Vertex(uint id)
 	: vertex_id_(id)
-	, edge_weight_(weight)
+	, list_()
+	, visited_(false)
 {}
 
-sdz::Vertex::~Vertex()
+SDZ::Vertex::~Vertex()
 {}
+
+void SDZ::Vertex::AddEdge(uint destination, uint weight)
+{
+	Edge new_edge = Edge(vertex_id_, destination, weight);
+	list_.PushBack(new_edge);
+}
+
+void SDZ::Vertex::DisplayList()
+{
+	std::cout << "Adjacency List for Vertex " << vertex_id_ << std::endl;
+	for (auto it = list_.begin(); it != list_.end(); it++)
+		std::cout << it->destination_id << ":" << it->weight_ << " ";
+	std::cout << std::endl;
+}
+
+SDZ::Vertex & SDZ::Vertex::operator=(const Vertex &other)
+{
+	vertex_id_ = other.vertex_id_;
+	visited_ = other.visited_;
+	list_ = std::move(other.list_);
+	return *this;
+}
+
+
--- SDiZO Zadanie 2/SDiZO Zadanie 2/Vertex.h
+++ SDiZO Zadanie 2/SDiZO Zadanie 2/Vertex.h
@@ -1,17 +1,32 @@
 #pragma once
+#include "List.h"
+#include "Edge.h"
+#include <iostream>
 
 typedef unsigned int uint;
-namespace sdz
+
+
+namespace SDZ
 {
 	class Vertex
 	{
 	public:
 
 		uint vertex_id_;
-		uint edge_weight_;
+		bool visited_;
+		DTS::List<SDZ::Edge> list_;
+
+		uint x_;
+		uint y_;
+
 		Vertex();
-		Vertex(uint id, uint weight);
+		Vertex(uint id);
 		~Vertex();
+
+		void AddEdge(uint destination, uint weight);
+		void DisplayList();
+
+		Vertex & operator = (const Vertex&);
 	};
 
 
