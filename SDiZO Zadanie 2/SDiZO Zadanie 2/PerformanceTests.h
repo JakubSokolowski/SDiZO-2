7268e6a208c26ee854fd88482533d8de1379499e
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2017-05-14 20:12:03 +0200

    Add sample Generation

--- SDiZO Zadanie 2/SDiZO Zadanie 2/PerformanceTests.h
+++ SDiZO Zadanie 2/SDiZO Zadanie 2/PerformanceTests.h
@@ -2,8 +2,9 @@
 #include <Windows.h>
 #include <iostream>
 #include <iomanip>
+#include <string>
 #include "AdjacencyListGraph.h"
-
+#include "IncidenceMatrixGraph.h"
 using namespace SDZ;
 //Processor Frequency
 
@@ -57,4 +58,69 @@ void AStarHeuristicComparision()
 void HeapComparisionTest()
 {
 
+}
+
+void GenerateTestSamples()
+{
+	//Size 25, 50, 100, 200, 400
+	//Density 25,50,75,99
+	std::string filepath = "C:\\Users\\Jakub\\Documents\\IV Semestr\\Struktury Danych i Z³o¿onoœc Obliczeniowa\\Projekt\\SDiZO Zadanie 2\\SDiZO Zadanie 2\\TestSamples\\25Density\\";
+	std::string s = "V";
+	std::string den = "D";
+	std::string post = ".txt";
+
+
+	double density = 0.25;
+
+	for (int i = 50; i < 801; i *= 2)
+	{
+		for (int it = 0; it < 5; it++)
+		{
+			std::string filename = filepath + s + std::to_string(i) + den + std::to_string(int(density * 100)) + std::to_string(it) + post;
+			AdjacencyListGraph graph = AdjacencyListGraph(i, density, true, true, true);
+			graph.WriteToFile(filename);
+			std::cout << "Finished writing: " + filename << std::endl;
+		}
+
+	}
+
+	density = 0.50;
+	filepath = "C:\\Users\\Jakub\\Documents\\IV Semestr\\Struktury Danych i Z³o¿onoœc Obliczeniowa\\Projekt\\SDiZO Zadanie 2\\SDiZO Zadanie 2\\TestSamples\\50Density\\";
+	for (int i = 50; i < 801; i *= 2)
+	{
+		for (int it = 0; it < 5; it++)
+		{
+			std::string filename = filepath + s + std::to_string(i) + den + std::to_string(int(density * 100)) + std::to_string(it) + post;
+			AdjacencyListGraph graph = AdjacencyListGraph(i, density, true, true, true);
+			graph.WriteToFile(filename);
+			std::cout << "Finished writing: " + filename << std::endl;
+		}
+
+	}
+	density = 0.75;
+	filepath = "C:\\Users\\Jakub\\Documents\\IV Semestr\\Struktury Danych i Z³o¿onoœc Obliczeniowa\\Projekt\\SDiZO Zadanie 2\\SDiZO Zadanie 2\\TestSamples\\75Density\\";
+	for (int i = 50; i < 801; i *= 2)
+	{
+		for (int it = 0; it < 5; it++)
+		{
+			std::string filename = filepath + s + std::to_string(i) + den + std::to_string(int(density * 100)) + std::to_string(it) + post;
+			AdjacencyListGraph graph = AdjacencyListGraph(i, density, true, true, true);
+			graph.WriteToFile(filename);
+			std::cout << "Finished writing: " + filename << std::endl;
+		}
+
+	}
+	density = 0.99;
+	filepath = "C:\\Users\\Jakub\\Documents\\IV Semestr\\Struktury Danych i Z³o¿onoœc Obliczeniowa\\Projekt\\SDiZO Zadanie 2\\SDiZO Zadanie 2\\TestSamples\\99Density\\";
+	for (int i = 50; i < 801; i *= 2)
+	{
+		for (int it = 0; it < 5; it++)
+		{
+			std::string filename = filepath + s + std::to_string(i) + den + std::to_string(int(density * 100)) + std::to_string(it) + post;
+			AdjacencyListGraph graph = AdjacencyListGraph(i, density, true, true, true);
+			graph.WriteToFile(filename);
+			std::cout << "Finished writing: " + filename << std::endl;
+		}
+
+	}
 }
