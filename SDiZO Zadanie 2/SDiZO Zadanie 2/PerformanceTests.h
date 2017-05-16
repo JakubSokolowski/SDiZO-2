d5201ce57551788920021fd6239b723d3d12019a
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2017-05-16 11:17:51 +0200

    Add FileExists Method, Add testing method for all aglorithms and representations

--- SDiZO Zadanie 2/SDiZO Zadanie 2/PerformanceTests.h
+++ SDiZO Zadanie 2/SDiZO Zadanie 2/PerformanceTests.h
@@ -1,18 +1,29 @@
 #pragma once
 #include <Windows.h>
+#include <random>
+#include <fstream>
 #include <iostream>
 #include <iomanip>
 #include <string>
 #include "AdjacencyListGraph.h"
 #include "IncidenceMatrixGraph.h"
+
 using namespace SDZ;
+
+
+bool FileExits(std::string &filename)
+{
+
+	struct stat buffer;
+	return (stat(filename.c_str(), &buffer) == 0);
+}
 //Processor Frequency
 
 double PCFreq = 0.0;
 __int64 CounterStart = 0;
 
 //ms - 1000.0
-double time_unit = 1.0;
+double time_unit = 1000.0;
 void StartCounter()
 {
 	LARGE_INTEGER li;
@@ -32,7 +43,24 @@ double GetCounter()
 	return double(li.QuadPart - CounterStart) / PCFreq;
 }
 
-void AdjacencyMatrixTest()
+void RandomPath(uint &start, uint &finish, uint max_vertex)
+{
+	std::random_device rd;
+	std::mt19937 rng(rd());
+	std::uniform_int_distribution<int> uni(0, max_vertex - 1);
+	bool not_the_same = false;
+	while (!not_the_same)
+	{
+		start = uni(rng);
+		finish = uni(rng);
+		if (start != finish)
+		{
+			not_the_same = true;
+		}
+	}
+}
+
+void AdjacencyListTest()
 {
 	// Ford - fulkerson. For graph below Max flow should be 23
 
@@ -106,6 +134,7 @@ void AdjacencyMatrixTest()
 	std::cout << "\n\nShortest path from 0 to 7: " << test_graph.AStarDistanceSearch(0, 5, SDZ::DIJKSTRA);
 
 }
+
 void IncidenceMatrixTest()
 {
 	// Ford - fulkerson. For graph below Max flow should be 23
@@ -174,9 +203,9 @@ void IncidenceMatrixTest()
 void AStarHeuristicComparision()
 {
 	uint source = 12;
-	uint destination = 456;
+	uint destination = 345;
 
-	AdjacencyListGraph graph = AdjacencyListGraph(1000, 0.3, true, true);
+	AdjacencyListGraph graph = AdjacencyListGraph(500, 0.3, true, true);
 
 	StartCounter();
 	std::cout << "\nShortest path from " << source << " to " << destination << " : "
@@ -191,7 +220,7 @@ void AStarHeuristicComparision()
 	StartCounter();
 	std::cout << "\nShortest path from " << source << " to " << destination << " : "
 		<< graph.AStarDistanceSearch(source, destination, Heuristic::MANHATTAN) << std::endl;
-	std::cout << "Euclidean heuristic time: " << GetCounter() << std::endl;
+	std::cout << "Manhattan heuristic time: " << GetCounter() << std::endl;
 }
 
 void HeapComparisionTest()
@@ -263,3 +292,278 @@ void GenerateTestSamples()
 
 	}
 }
+
+void PrimFibHeap()
+{
+
+}
+void PrimBinHeap()
+{
+
+}
+
+void PrimTest()
+{
+	const int graph_vertices[5] = { 25, 50, 100, 200, 400 };
+	const double graph_density[4] = { 0.25, 0.50, 0.75, 0.99 };
+	AdjacencyListGraph graph = AdjacencyListGraph();
+
+	std::string filename = "PrimFibHeap.txt";
+	std::string filename2 = "PrimBinHeap.txt";
+	//Adj List Prim MST test
+	std::ofstream file(filename);
+	std::ofstream file2(filename2);
+
+	double fib_time = 0.0;
+	double bin_time = 0.0;
+
+	file << "Prim fib test" << "\n";
+	file2 << "Prim bin test" << "\n";
+
+	std::cout << "Prim MST Start\n";
+	for (int i = 0; i < 4; i++)
+	{
+		std::cout << "\nStart for graph with density " << graph_density[i];
+		for (int j = 0; j < 5; j++)
+		{
+			std::cout << "\n\tand vertices " << graph_vertices[j];
+			for (int k = 0; k < 10; k++)
+			{
+				graph.SetParameters(graph_vertices[j], graph_density[i], false, true);
+				StartCounter();
+				graph.PQPrimMST();
+				bin_time += GetCounter();
+				StartCounter();
+				graph.FHPrimMST();
+				fib_time += GetCounter();
+			}
+			//file << time / 10 << " " << "V" << graph_vertices[i] << "D" << graph_density[j] << "\n";
+			file << std::fixed << fib_time / 10 << "\n";
+			file2 << std::fixed << bin_time / 10 << "\n";
+			fib_time = 0.0;
+			bin_time = 0.0;
+		}
+	}
+	std::cout << "\nFinished Primm\n ";
+	file.close();
+	file2.close();
+}
+void AstarList()
+{
+	const uint graph_vertices[5] = { 25, 50, 100, 200, 400 };
+	const double graph_density[4] = { 0.25, 0.50, 0.75, 0.99 };
+	AdjacencyListGraph graph = AdjacencyListGraph();
+
+	std::string filename = "ListDijkstra.txt";
+	std::string filename2 = "ListAstar.txt";
+	//Adj List Prim MST test
+	std::ofstream file(filename);
+	std::ofstream file2(filename2);
+
+	double astar_time = 0.0;
+	double dijkstra_time = 0.0;
+
+	std::random_device rd;
+	std::mt19937 rng(rd());
+
+	uint start, finish, result;
+
+	file << "Adjacency list Dijkstra test" << "\n";
+	file2 << "Adjacency list Astar test" << "\n";
+
+	std::cout << "Astar & Dijkstra Adjacency list Start\n";
+	for (int i = 0; i < 4; i++)
+	{
+		std::cout << "\nStart for graph with denisty " << graph_density[i];
+		for (int j = 0; j <5; j++)
+		{
+			std::cout << "\n\tand vertices " << graph_vertices[j];
+			graph.SetParameters(graph_vertices[j], graph_density[i], true, true);
+			for (int k = 0; k < 10; k++)
+			{
+				RandomPath(start, finish, graph_vertices[j]);
+				StartCounter();
+				result = graph.AStarDistanceSearch(start, finish, Heuristic::EUCLIDEAN);
+				astar_time += GetCounter();
+				StartCounter();
+				result = graph.AStarDistanceSearch(start, finish, Heuristic::DIJKSTRA);
+				dijkstra_time += GetCounter();
+			}
+			//file << time / 10 << " " << "V" << graph_vertices[i] << "D" << graph_density[j] << "\n";
+			std::cout <<" "<< astar_time << " " << dijkstra_time << std::endl;
+			file << std::fixed << dijkstra_time / 10 << "\n";
+			file2 << std::fixed << astar_time / 10 << "\n";
+			astar_time = 0.0;
+			dijkstra_time = 0.0;
+		}
+	}
+	std::cout << "\n A* Adjacency List Finish\n";
+	file.close();
+	file2.close();
+
+}
+void AstarMatrix()
+{
+	const uint graph_vertices[5] = { 25, 50, 100, 200, 400 };
+	const double graph_density[4] = { 0.25, 0.50, 0.75, 0.99 };
+	IncidenceMatrixGraph graph = IncidenceMatrixGraph();
+
+	std::string filename = "MatrixDijkstra.txt";
+	std::string filename2 = "MatrixAstar.txt";
+	//Adj List Prim MST test
+	std::ofstream file(filename);
+	std::ofstream file2(filename2);
+
+	double astar_time = 0.0;
+	double dijkstra_time = 0.0;
+
+	std::random_device rd;
+	std::mt19937 rng(rd());
+
+	uint start, finish, result;
+
+	file << "Incidence matrix Dijkstra test" << "\n";
+	file2 << "Incidence matrix  A* test" << "\n";
+
+	std::cout << "Astar & Dijkstra Incidence matrix list Start\n";
+	for (int i = 0; i < 4; i++)
+	{
+		std::cout << "\nStart for graph with denisty " << graph_density[i];
+		for (int j = 0; j <5; j++)
+		{
+			std::cout << "\n\tand vertices " << graph_vertices[j];
+			graph.SetParameters(graph_vertices[j], graph_density[i], true, true);
+			for (int k = 0; k < 10; k++)
+			{
+				RandomPath(start, finish, graph_vertices[j]);
+				StartCounter();
+				result = graph.AStarDistanceSearch(start, finish, Heuristic::EUCLIDEAN);
+				astar_time += GetCounter();
+				StartCounter();
+				result = graph.AStarDistanceSearch(start, finish, Heuristic::DIJKSTRA);
+				dijkstra_time += GetCounter();
+			}
+			//file << time / 10 << " " << "V" << graph_vertices[i] << "D" << graph_density[j] << "\n";
+			std::cout << " " << astar_time/10 << " " << dijkstra_time/10 << std::endl;
+			file << std::fixed << dijkstra_time / 10 << "\n";
+			file2 << std::fixed << astar_time / 10 << "\n";
+			astar_time = 0.0;
+			dijkstra_time = 0.0;
+		}
+	}
+	std::cout << "\n A* Adjacency List Finish\n";
+	file.close();
+	file2.close();
+
+}
+void AStarTest()
+{
+	AstarList();
+	AstarMatrix();
+}
+
+void FordFulkersonList()
+{
+	const uint graph_vertices[5] = { 25, 50, 100, 200, 400 };
+	const double graph_density[4] = { 0.25, 0.50, 0.75, 0.99 };
+	AdjacencyListGraph graph = AdjacencyListGraph();
+
+	std::string filename = "ListFordFulkerson.txt";
+	//Adj List Prim MST test
+	std::ofstream file(filename);
+	double time = 0.0;
+
+
+
+	std::random_device rd;
+	std::mt19937 rng(rd());
+
+	uint start, finish, result;
+
+	//file << "Adjacency list Ford Fulkerson test" << "\n";
+
+	std::cout << "\nFord Fulkerson Adjacency List Start\n";
+
+	for (int i = 0; i < 4; i++)
+	{
+		std::cout << "\nStart for graph with denisty " << graph_density[i];
+		for (int j = 0; j < 5; j++)
+		{
+			std::cout << "\n\tand vertices " << graph_vertices[j];
+			graph.SetParameters(graph_vertices[j], graph_density[i], true, true);
+			for (int k = 0; k < 10; k++)
+			{
+				RandomPath(start, finish, graph_vertices[j]);
+
+				StartCounter();
+				result = graph.FordFulkerson(start, finish);
+				time += GetCounter();
+			}
+			file << std::fixed << time / 10 << "\n";
+			time = 0.0;
+		}
+	}
+	std::cout << "\n Ford Fulkerson Adjacency List Finish\n";
+	file.close();
+
+}
+void FordFulkersonMatrix()
+{
+	const uint graph_vertices[5] = { 25, 50, 100, 200, 400 };
+	const double graph_density[4] = { 0.25, 0.50, 0.75, 0.99 };
+	IncidenceMatrixGraph graph = IncidenceMatrixGraph();
+
+	std::string filename = "MatrixFordFulkerson.txt";
+	//Adj List Prim MST test
+	std::ofstream file(filename);
+	double time = 0.0;
+
+
+
+	std::random_device rd;
+	std::mt19937 rng(rd());
+
+	uint start, finish, result;
+
+	//file << "Adjacency list Ford Fulkerson test" << "\n";
+
+	std::cout << "\nFord Fulkerson Incidence Matrix Start\n";
+
+	for (int i = 0; i < 4; i++)
+	{
+		std::cout << "\nStart for graph with denisty " << graph_density[i];
+		for (int j = 0; j < 5; j++)
+		{
+			std::cout << "\n\tand vertices " << graph_vertices[j];
+			graph.SetParameters(graph_vertices[j], graph_density[i], true, true);
+			for (int k = 0; k < 5; k++)
+			{
+				RandomPath(start, finish, graph_vertices[j]);
+
+				StartCounter();
+				result = graph.FordFulkerson(start, finish);
+				time += GetCounter();
+			}
+			file << std::fixed << time / 5 << "\n";
+			time = 0.0;
+		}
+	}
+	std::cout << "\n Ford Fulkerson Adjacency List Finish\n";
+	file.close();
+
+}
+void FordFulkersonTest()
+{
+	//FordFulkersonList();
+	FordFulkersonMatrix();
+}
+
+
+void TimeAlgorithms()
+{
+	//PrimTest();
+	//AStarTest();
+	FordFulkersonTest();
+}
+
+
